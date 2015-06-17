0151dbbac88929d46e59303fa69b695fa697a608
Keon Cummings <keon.cummings@gmail.com>
2015-06-17 14:17:25 -0500

    everything is fixed and works

--- public/javascripts/combineQuotes.js
+++ public/javascripts/combineQuotes.js
@@ -1,2 +1,10 @@
-var quotes = require('quotes');
+fs = require('fs');
+var quotes = require('./quotes.js');
+var exportedQuotes = quotes.allQuotes();
+var fullQuote = {};
 
+for(i in exportedQuotes.author) {
+	fullQuote[exportedQuotes.author[i]] = exportedQuotes.quote[i];
+}
+
+fs.writeFile('allQuotes.js', JSON.stringify(fullQuote, null, 4));
