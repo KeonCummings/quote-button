391e298e6b488c41ffab14f607e59b5304eacd35
Keon Cummings <keon.cummings@gmail.com>
2015-06-19 18:51:04 -0500

    fixed duplicate quote issue

--- public/javascripts/combineQuotes.js
+++ public/javascripts/combineQuotes.js
@@ -1,10 +1,24 @@
 fs = require('fs');
 var quotes = require('./quotes.js');
 var exportedQuotes = quotes.allQuotes();
+var allAuthors = exportedQuotes.author;
+var uniqs = {};
 var fullQuote = {};
+for(var i = 0; i < allAuthors.length ; i++){
+		fullQuote[allAuthors[i]] = null;
+}
+var uniqAuthors = Object.keys(uniqs);
 
-for(i in exportedQuotes.author) {
-	fullQuote[exportedQuotes.author[i]] = exportedQuotes.quote[i];
+for(var i = 0; i < Object.keys(fullQuote).length; i++){
+	for(var j = 0; j < exportedQuotes.author.length; j++){
+		if(Object.keys(fullQuote)[i] == exportedQuotes.author[j]){
+			if(fullQuote[exportedQuotes.author[j]] == null){
+				fullQuote[exportedQuotes.author[j]] = [exportedQuotes.quote[j]]
+			} else {
+				fullQuote[exportedQuotes.author[j]].push(exportedQuotes.quote[j])
+			}
+		}
+	}
 }
 
 fs.writeFile('allQuotes.js', JSON.stringify(fullQuote, null, 4));
